fec978f076daad4b4eff6923c155ede3ce2ffe84
Indra Saputra <55437072+indrasp@users.noreply.github.com>
2020-09-23 21:12:45 +0700

    membuat contoh definisi fungsi

---
+++ minggu-02/src/defining-functions.py
@@ -0,0 +1,26 @@
+#Kita dapat membuat fungsi yang menulis seri Fibonacci ke batas acak arbitrary:
+print("fungsi pada Fibonacci")
+def fib(n):    # write Fibonacci series up to n
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+# Now call the function we just defined:
+fib(2000)
+
+print("fungsi mengembalikan list nomor Fibonacci")
+
+def fib2(n):  # return Fibonacci series up to n
+    """Return a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)    # see below
+        a, b = b, a+b
+    return result
+
+f100 = fib2(100)    # call it
+print(f100)               # write the result
